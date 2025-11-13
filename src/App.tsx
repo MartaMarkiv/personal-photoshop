64449aecb937651b6126503893933be59d20969f
Marta Markiv <mpat@keplercode.com>
2025-11-13 11:44:17 +0200

    Added canva to draw mask

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,57 @@
-import { useState, type ChangeEvent } from "react";
+import { useEffect, useRef, useState, type ChangeEvent } from "react";
 import "./App.css";
 
 function App() {
-  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const imgRef = useRef<HTMLImageElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
+  const [drawing, setDrawing] = useState(false);
 
-  const uploadImeg = (event: ChangeEvent<HTMLInputElement>) => {
+  useEffect(() => {
+    if (selectedImage && imgRef.current && canvasRef.current) {
+      const img = imgRef.current;
+      const canvas = canvasRef.current;
+
+      canvas.width = img.naturalWidth;
+      canvas.height = img.naturalHeight;
+
+      canvas.style.width = `${img.width}px`;
+      canvas.style.height = `${img.height}px`;
+
+      ctxRef.current = canvas.getContext("2d");
+    }
+  }, [selectedImage]);
+
+  const handleMouseDown = () => setDrawing(true);
+
+  const handleMouseUp = () => setDrawing(false);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!drawing) return;
+    const ctx = canvasRef.current?.getContext("2d");
+    if (!ctx) return;
+
+    const rect = canvasRef.current!.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fill();
+  };
+
+  const handleEdit = async () => {
+    if (!selectedImage || !canvasRef.current) return;
+  };
+
+  const uploadImage = (event: ChangeEvent<HTMLInputElement>) => {
     const fileList = event.target.files;
     if (fileList) {
-      setSelectedImage(fileList[0]);
+      const reader = new FileReader();
+      reader.onload = () => setSelectedImage(reader.result as string);
+      reader.readAsDataURL(fileList[0]);
     } else {
       setSelectedImage(null);
     }
@@ -22,14 +66,31 @@ function App() {
           type="file"
           title="Browse image"
           placeholder="Upload image"
-          onChange={uploadImeg}
+          onChange={uploadImage}
           accept="image/*"
         />
       </div>
+      {selectedImage && (
+        <div className="image-edit-container">
+          <img
+            ref={imgRef}
+            src={selectedImage}
+            alt="Selected image"
+            className="selected-image"
+          />
+          <canvas
+            ref={canvasRef}
+            className="masked-image"
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+            onMouseMove={handleMouseMove}
+          />
+        </div>
+      )}
       <div>
-        {selectedImage && (
-          <img src={URL.createObjectURL(selectedImage)} alt="Image" />
-        )}
+        <button onClick={handleEdit} disabled={!selectedImage}>
+          Edit
+        </button>
       </div>
     </>
   );
